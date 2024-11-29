65121c41791ea036eee2e47ce0d484c0e7822a03
Macbook Pro <macbookpro@192.168.1.3>
2024-11-29 23:23:31 +0500

    updating

---
+++ arithmetic-operations.py
@@ -0,0 +1,14 @@
+# print function
+
+names = "Abdurahim\'s notebook"
+
+print(f"{names} Ubaydov 2006-yilda tug'ulgan")
+
+
+my_name = 'Abdurahim'
+
+
+if my_name != 'Abdurahim':
+    print('hello world')
+else:
+    print('salom dunyo')
---
+++ variables.py
@@ -0,0 +1,15 @@
+ism = 'Abdurahim'
+familia = 'Ubaydov'
+
+print(ism + ' ' + familia) # it returns same as Abdurahim Ubaydov, but it's bit of complicated
+print(f"{ism} {familia}") # that's kinda easier option
+print(2**2) # ** it shows square 
+
+
+_ism = 'Abdurahim'
+
+full_name = 'Abdurahim Ubaydov'
+print(full_name)
+
+
+print('slom')
