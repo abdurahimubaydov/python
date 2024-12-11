1b68640aa7ce5899ede932d7498c0d7fdd7354ee
Macbook Pro <macbookpro@192.168.1.4>
2024-12-11 09:50:54 +0500

    updating

---
+++ while.py
@@ -0,0 +1,26 @@
+# while - means up to. For example, means up to five
+
+# numb = 0
+# while numb<=11:
+#     print(numb)
+#     numb+=1
+
+
+# practise - 1
+# books = []
+# while True:
+#     value = input("Enter your favourite books, (If you wanna stop, just write 'stop'): ")
+#     if value.lower() == 'stop': 
+#         break
+#     books.append(value)
+
+# print('\nYour favourite books 👇')
+# for book in books:
+#     print(book)
+
+
+
+# practise - 2
+
+value = input('Enter your age, ')
+value += "(if you wanna quit write `quit` or 'exit')"
