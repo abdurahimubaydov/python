f35729df3f12a589f3922c41fa4e6ae654ff257e
Macbook Pro <macbookpro@192.168.1.4>
2024-12-15 01:24:37 +0500

    last updating

---
+++ all-practises.py
@@ -0,0 +1,32 @@
+# while True:
+#     msg = input('Enter your favourite book (write stop in you want): ',)
+#     if msg == 'stop':
+#         break
+
+
+# qst = 'enter your age,'
+# qst += " write 'quit' or 'exit' to quit "
+# while True:
+#     age = int(input(qst))
+#     amount = 0
+
+#     if age < 7:
+#         amount = 2000
+#     elif age >= 7 and age < 18:
+#         amount = 3000
+#     elif age >= 18 and 65 > age:
+#         amount = 10000
+#     else: 0
+
+#     if amount == 0: print('szga bepul')
+#     else: print(f" chipta narxi {amount} so'm ")
+
+    
+# products = []
+# while True:
+#     quest = input('add some product: ')
+#     products.append(quest)
+
+#     asking = input("wanna add another product? 'yes' / 'not' ")
+#     if asking == 'not': break
+# print(products)
